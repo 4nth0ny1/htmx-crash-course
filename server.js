9a174d50e7f90536aa0053d3df5b4b5a23aac34d
4nth0ny1 <acatullo4@gmail.com>
2024-01-18 13:46:02 -0600

    now limited the amount of results with htmx

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(express.json());
 // routes
 app.get("/users", async (req, res) => {
   setTimeout(async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const limit = +req.query.limit || 10;
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
     const users = await response.json();
     //   const usas = [{ id: 1, name: "jar jar" }];
     res.send(`
